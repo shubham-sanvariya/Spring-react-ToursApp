d531ead5979e5d8095d49a0fccca3a8c7abf7130
shubham-sanvariya <shubhamsanvariyait.com>
2023-03-23 23:19:36 +0530

    Initial commit

---
+++ src/main/java/shubh/SpringFramework/ToursApp/ToursAppApplication.java
@@ -0,0 +1,13 @@
+package shubh.SpringFramework.ToursApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToursAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToursAppApplication.class, args);
+	}
+
+}
